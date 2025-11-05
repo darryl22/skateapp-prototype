a71cce25bb33666831e507e463f592baa065fd3e
darryl22 <darrylandrew22@gmail.com>
2025-11-05 17:43:17 +0300

    map, auth and other minor changes

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const fs = require("fs")
 const Cryptr = require("cryptr")
 const cryptr = new Cryptr(process.env.ENCRYPTION_KEY)
 const cors = require("cors")
-const {MongoClient} = require("mongodb")
 const session = require("express-session")
+const {MongoClient, ObjectId} = require("mongodb")
 const MongoDBStore  = require("connect-mongodb-session")(session)
 const store = new MongoDBStore ({
     uri: "mongodb://localhost:27017/skateapp",
@@ -19,6 +19,8 @@ const store = new MongoDBStore ({
 
 const uri = process.env.MONGO_URI
 const client = new MongoClient(uri)
+const DatabaseMethods = require("./dbFunctions")
+let databaseMethods = new DatabaseMethods()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -46,54 +48,45 @@ app.get('/', (request, response) => {
     response.render('index.ejs', {user: request.session.user})
 })
 
-app.get('/map', (request, response) => {
-    const date = new Date()
-    if (request.session.user === undefined) {
-        request.session.user = "anonymous"
-    } else {
-        response.locals.user = request.session.user
-    }
-
-    async function db() {
-        try{
-            const database = client.db("skateapp")
-            const spots = database.collection("spots")
-            let res = await spots.find().toArray()
-            // console.log(res)
-            response.render('map.ejs', {mapboxtoken : process.env.MAPBOX_ACCESS_TOKEN, user: request.session.user, spots: res})
-        } catch (error) {
-            console.log(error)
-            response.redirect("/")
+app.get('/map', async (request, response) => {
+    await Promise.all([databaseMethods.getMany("spots"), databaseMethods.getOne("users", {email: request.session.user})])
+    .then(res => {
+        console.log(res[1])
+        let userObject = {
+            username: "anonymous",
         }
-    }
-    db()
+        if (res[1] !== null) {
+            userObject.username = res[1].username
+            userObject.preferences = res[1].preferences
+        }
+        response.render('map.ejs', {mapboxtoken : process.env.MAPBOX_ACCESS_TOKEN, user: userObject, spots: res[0]})
+    })
+    .catch(error => {
+        console.log(error)
+        response.redirect("/")
+    })
 })
 
-app.post('/addspot', (request, response) => {
-    console.log(request.body.spotimages)
+app.post('/addspot', async (request, response) => {
     const date = new Date()
-    // console.log(date)
-    async function db() {
-        try {
-            const database = client.db("skateapp")
-            const spots = database.collection("spots")
-            const res = await spots.insertOne({
-                description: request.body.description,
-                spottype: request.body.spottype,
-                longitude: request.body.lng,
-                latitude: request.body.lat,
-                spotimages: request.body.spotimages,
-                createdAt: date
-            })
-            console.log(res)
-        } catch (error) {
-            console.log(error)
-        }
+    let data = {
+        description: request.body.description,
+        spottype: request.body.spottype,
+        longitude: request.body.lng,
+        latitude: request.body.lat,
+        spotimages: request.body.spotimages,
+        createdAt: date,
+        createdBy: request.session.user
     }
-    db()
+    await databaseMethods.addOne("spots", data)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(error => {
+        console.log(error)
+        response.redirect("/map")
+    })
 
-    // const reader = fs.readFileSync(request.body.images[0], {encoding: 'base64'})
-    // console.log(reader)
     response.redirect("/map")
 })
 
@@ -101,79 +94,136 @@ app.get('/info', (request, response) => {
     response.render('info.ejs')
 })
 
-app.get('/profile', (request, response) => {
-    async function db() {
-        try {
-            const database = client.db("skateapp")
-            const users = database.collection("users")
-            const res = await users.findOne({email: request.session.user})
-            console.log(res._id.toString())
-            
-        } catch (err){
-            console.log(err)
-            response.redirect("/login")
+app.get('/profile', async (request, response) => {
+    await databaseMethods.getOne("users", {email: request.session.user})
+    .then(res => {
+        userProfile = {
+            username: res.username,
+            email: res.email,
+            preferences: {
+                param1: res.preferences.param1,
+                param2: res.preferences.param2,
+                param3: res.preferences.param3
+            },
+            preferences2: {
+                param1: res.preferences2.param1,
+                param2: res.preferences2.param2,
+                param3: res.preferences2.param3
+            }
         }
+        response.render('profile.ejs', {user: userProfile})
+    })
+})
+
+app.post('/updateProfile', async (request, response) => {
+    try{
+        userID = request.session.userID
+        idObject = ObjectId.createFromHexString(userID)
+        user = await databaseMethods.getOne("users", {_id: idObject})
+        console.log(request.body)
+        update = await databaseMethods.makeUpdate("users", {_id: idObject}, {
+            $set: {
+                username: "user556",
+                preferences: {
+                    param1: request.body.param1 === "true" ? true : false,
+                    param2: request.body.param2 === "true" ? true : false,
+                    param3: request.body.param3 === "true" ? true : false
+                },
+                preferences2: {
+                    param1: request.body.param4 === "true" ? true : false,
+                    param2: request.body.param5 === "true" ? true : false,
+                    param3: request.body.param6 === "true" ? true : false
+                }
+            }
+        })
+        console.log("User update", update)
+        // console.log("UserId", request.session.userID)
+        // console.log(user)
+        // response.redirect("/profile")
+        response.json({success: "true"})
+    } catch (error) {
+        console.log(error)
+        // response.redirect("/profile")
+        response.json({
+            success: "false"
+        })
     }
-    db()
-    response.render('profile.ejs', {user: request.session.user})
 })
 
 app.get('/login', (request, response) => {
     response.render('login.ejs', {user: request.session.user})
 })
 
-app.post('/login', (request, response) => {
+app.post('/login', async (request, response) => {
     const password = request.body.password
-    async function db() {
-        try {
-            const database = client.db("skateapp")
-            const users = database.collection("users")
-            const res = await users.findOne({email: request.body.email})
-            console.log(cryptr.decrypt(res.password))
-            if(password === cryptr.decrypt(res.password)) {
-                request.session.user = res.email
-                response.redirect("/profile")
-            } else {
-                console.log("wrong password")
-                response.redirect("/login")
-            }
-        } catch (err){
-            console.log(err)
+    await databaseMethods.getOne("users", {email: request.body.email})
+    .then(res => {
+        console.log(cryptr.decrypt(res.password))
+        if(password === cryptr.decrypt(res.password)) {
+            request.session.user = res.email
+            request.session.userID = res._id.toString()
+            response.redirect("/profile")
+        } else {
+            console.log("wrong password")
             response.redirect("/login")
         }
-    }
-    db()
+    })
+    .catch(error => {
+        console.log(error)
+        response.redirect("/login")
+    })
 })
 
 app.get('/signup', (request, response) => {
     response.render('signup.ejs', {user: request.session.user})
 })
 
-app.post('/signup', (request, response) => {
+app.post('/signup', async (request, response) => {
     const username = request.body.username
     const email = request.body.email
     const password = request.body.password
     const confirmpassword = request.body.confirmpassword
     if (password === confirmpassword) {
-        const encryptedpass = cryptr.encrypt(password)
-        async function adduser() {
-            try {
-                const database = client.db("skateapp")
-                const users = database.collection("users")
+        await Promise.all([databaseMethods.getOne("users", {email: email}), databaseMethods.getOne("users", {username: username})])
+        .then(res => {
+            console.log("Check users", res)
+            if (res[0] === null && res[1] === null) {
+                const encryptedpass = cryptr.encrypt(password)
                 const user = {
                     username: username,
                     email: email,
-                    password: encryptedpass
+                    password: encryptedpass,
+                    preferences: {
+                        param1: true,
+                        param2: true,
+                        param3: true
+                    },
+                    preferences2: {
+                        param1: true,
+                        param2: false,
+                        param3: false
+                    }
                 }
-                const result = await users.insertOne(user)
-                console.log("document inserted")
-            } catch(error) {
-                console.log(error)
+                // await databaseMethods.addOne("users", user)
+                // .then(res => {
+                //     console.log(res)
+                //     response.redirect("/profile")
+                // })
+                // .catch(error => {
+                //     console.log(error)
+                //     response.redirect("/signup")
+                // })
+                response.redirect("/signup")
+            } else if (res[0] !== null) {
+                console.log("This email already exists")
+                response.redirect("/signup")
+            } else if (res[1] !== null) {
+                console.log("This username already exists")
+                response.redirect("/signup")
             }
-        }
-        adduser()
-        response.redirect("/profile")
+        })
     } else {
+        console.log("Password Missmatch")
         response.redirect("/signup")
     }
 })
---
+++ dbFunctions.js
@@ -0,0 +1,66 @@
+const {MongoClient} = require("mongodb")
+const uri = process.env.MONGO_URI
+const client = new MongoClient(uri)
+
+class DatabaseMethods {
+    async addOne(col, object) {
+        try{
+            const database = client.db("skateapp")
+            const collection = database.collection(col)
+            const res = await collection.insertOne(object)
+            return res
+        } catch (error) {
+            console.log(error)
+            return {}
+        }
+    }
+
+    async addMultiple(col, object) {
+        try{
+            const database = client.db("skateapp")
+            const collection = database.collection(col)
+            const res = await collection.insertMany(object)
+            return res
+        } catch (error) {
+            console.log(error)
+            return {}
+        }
+    }
+
+    async getOne(col, object) {
+        try{
+            const database = client.db("skateapp")
+            const collection = database.collection(col)
+            const res = await collection.findOne(object)
+            return res
+        } catch (error) {
+            console.log(error)
+            return {}
+        }
+    }
+
+    async getMany(col) {
+        try{
+            const database = client.db("skateapp")
+            const collection = database.collection(col)
+            const res = await collection.find().toArray()
+            return res
+        } catch (error) {
+            console.log(error)
+            return {}
+        }
+    }
+
+    async makeUpdate(col, filter, update) {
+        try{
+            const database = client.db("skateapp")
+            const collection = database.collection(col)
+            const res = await collection.updateOne(filter, update)
+            return res
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports = DatabaseMethods
